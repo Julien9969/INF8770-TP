bb55b4da24b62cf071428c08f72ee65448483dc7
Julien9969 <julien.roux404@gmail.com>
2024-04-07 22:04:52 -0400

    code fin

--- TP3/neu_img_finder.py
+++ TP3/neu_img_finder.py
@@ -122,7 +122,6 @@ def neu_find(result_csv: csv.writer, folder=IMG_FOLDER):
     # save_vars_neu(101)
     print(f"F1_SCORE_NEU: {F1_SCORE_NEU}, N_IMAGES_CLEFS: {N_IMAGES_CLEFS}")
     try:
-        raise FileNotFoundError
         load_vars_neu()
         index_time = 'loaded from file'
         pass
